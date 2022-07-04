178e9f05d4789573821d344182c4b91400284216
MDT <MDT@DESKTOP-APL9J41>
2022-07-04 17:40:54 +0500

    Updated

--- MartManagement.BOL/PaymentType.cs
+++ MartManagement.BOL/PaymentType.cs
@@ -11,7 +11,8 @@ namespace MartManagement.BOL
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class PaymentType
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,6 +23,9 @@ namespace MartManagement.BOL
         }
     
         public int PaymentType_Id { get; set; }
+
+        [Required(ErrorMessage ="*")]
+        [Display(Name ="Payment Type Name")]
         public string PaymentType_Name { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
